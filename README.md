8728e601756c5842cf6ff2ea6e10502d3410d131
JohnOh26 <john.oh26@gmail.com>
2024-10-24 22:24:59 -0400

    made progress with lsps

--- README.md
+++ README.md
@@ -9,3 +9,8 @@ plan to get to tmux
 will see where to go from there. Maybe emacs. who knows :)
 
 please feel free to share your productivity setup tips/tricks/tools
+
+# Need to download language servers and set them to $PATH
+
+- https://luals.github.io/#neovim-install
+- pyright via npm
---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,34 @@
+return {
+    {
+        "williamboman/mason.nvim",
+        config = function()
+            require("mason").setup()
+        end
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        config = function()
+            require("mason-lspconfig").setup({
+                ensure_installed = {"lua_ls", "ts_ls", "omnisharp", "clangd", "pyright"}
+            })
+        end
+    },
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            local lspconfig = require("lspconfig")
+            lspconfig.lua_ls.setup({})
+            lspconfig.ts_ls.setup({})
+            lspconfig.omnisharp.setup({})
+
+            vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+            vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, {})
+            vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+            vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, {})
+            vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, {})
+            vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
+        end
+    }
+}
+
+
