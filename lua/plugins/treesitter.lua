8a946fd889c1b8a96904a6bca50b9ad8b919cd01
John Oh <41029893+johnoh26@users.noreply.github.com>
2024-11-03 13:52:54 -0500

    removed node modules; updated default space to 2; added none-ls

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,15 +1,14 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function () 
-        local configs = require("nvim-treesitter.configs")
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  config = function () 
+    local configs = require("nvim-treesitter.configs")
 
-        configs.setup({
-            ensure_installed = {"c_sharp", "c", "lua", "vim", "vimdoc", "query", "elixir", "heex","typescript", "javascript", "html" },
-            sync_install = false,
-            highlight = { enable = true },
-            indent = { enable = true },  
-            compilers ={"gcc"}
-        })
-    end
+    configs.setup({
+      autoinstall = true,
+      highlight = { enable = true },
+      indent = { enable = true },  
+      compilers ={"gcc"}
+    })
+  end
 }
--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -1,7 +1,8 @@
 
 vim.cmd("set expandtab")
-vim.cmd("set tabstop=4")
-vim.cmd("set softtabstop=4")
-vim.cmd("set shiftwidth=4")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.cmd("set scrolloff=999")
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
